5f319474c23a8b4ba19a2b36498fea69428df90d
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2024-10-08 11:51:41 +0300

    Update Membership component to remove class name comments

--- src/components/membership/membership.jsx
+++ src/components/membership/membership.jsx
@@ -19,7 +19,7 @@ export default function Membership() {
                     layout="intrinsic"
                 />
                 <div className={styles.memberShipSectionContentText}>
-                    <h6 className={styles.stepTitle}>1. Adım</h6> {/* Yeni sınıf ismi eklenmiştir */}
+                    <h6 className={styles.stepTitle}>1. Adım</h6>
                     <p className={styles.memberShipParagraph}>
                         Matematik derslerine ihtiyacın var mı? O halde doğru adrestesin! Lokasyon Matematik, ihtiyaçlarına en uygun ve konumuna en yakın matematik öğretmenini bulmanı sağlar. Matematik alanında geniş bir yelpazede; ilkokul takviyesi, sınav hazırlığı, ve üniversite dersleri gibi konularda ders alabileceğin en iyi öğretmenleri, gelişmiş arama motorumuz sayesinde dakikalar içinde keşfedebilirsin.
                     </p>
@@ -27,9 +27,9 @@ export default function Membership() {
             </div>
             <div className={styles.memberShipSectionContent}>
                 <div className={styles.memberShipSectionContentText}>
-                    <h6 className={styles.stepTitle}>2. Adım</h6> {/* Yeni sınıf ismi eklenmiştir */}
+                    <h6 className={styles.stepTitle}>2. Adım</h6>
                     <p className={styles.memberShipParagraph}>
-                        Öğretmenlerin tecrübeleri, uzmanlık alanları, ders verdiği yerler, tanıtım videoları, üniversite bilgileri ve saatlik ders ücretleri gibi bilgilere sayfamız üzerinden ulaşabilirsiniz. Birçok kritere göre karşılaştırma yaparak aradığınız matematik öğretmenini kolayca keşfedebilirsiniz! İhtiyaçlarınıza en uygun olduğunu düşündüğünüz matematik öğretmenine ders talebinizi iletin; öğretmeniniz size dönüş sağlasın, şartları konuşun ve derslere başlayın. Unutmayın, Özel Ders Alanı’nda her bütçeye uygun matematik öğretmeni bulunmaktadır. Saatlik fiyat aralığı 750 TL - 1.500 TL arasında değişmektedir.
+                        Öğretmenlerin tecrübeleri, uzmanlık alanları, ders verdiği yerler, tanıtım videoları, üniversite bilgileri ve saatlik ders ücretleri gibi bilgilere sayfamız üzerinden ulaşabilirsiniz. Birçok kritere göre karşılaştırma yaparak aradığınız matematik öğretmenini kolayca keşfedebilirsiniz! İhtiyaçlarınıza en uygun olduğunu düşündüğünüz matematik öğretmenine ders talebinizi iletin; öğretmeniniz size dönüş sağlasın, şartları konuşun ve derslere başlayın. Unutmayın, Özel Ders Alanı&rsquo;nda her bütçeye uygun matematik öğretmeni bulunmaktadır. Saatlik fiyat aralığı 750 TL - 1.500 TL arasında değişmektedir.
                     </p>
                 </div>
                 <Image
@@ -49,9 +49,9 @@ export default function Membership() {
                     layout="intrinsic"
                 />
                 <div className={styles.memberShipSectionContentText}>
-                    <h6 className={styles.stepTitle}>3. Adım</h6> {/* Yeni sınıf ismi eklenmiştir */}
+                    <h6 className={styles.stepTitle}>3. Adım</h6>
                     <p className={styles.memberShipParagraph}>
-                        Genel bir araştırma yapmak ve alternatif oluşturmak istiyorsanız, sizin için hazırladığımız kullanım kolaylığına sahip panelimizden tüm işlemlerinizi yönetebilirsiniz. "Genel Ders Talebi" oluşturma bölümünden sistemin sizi yönlendirdiği şekilde isteklerinizi net bir şekilde ifade ederek genel bir talep oluşturabilirsiniz. Talebinize en uygun matematik öğretmenleri ⚡ gün içerisinde sizinle iletişime geçecektir.
+                        Genel bir araştırma yapmak ve alternatif oluşturmak istiyorsanız, sizin için hazırladığımız kullanım kolaylığına sahip panelimizden tüm işlemlerinizi yönetebilirsiniz. &quot;Genel Ders Talebi&quot; oluşturma bölümünden sistemin sizi yönlendirdiği şekilde isteklerinizi net bir şekilde ifade ederek genel bir talep oluşturabilirsiniz. Talebinize en uygun matematik öğretmenleri ⚡ gün içerisinde sizinle iletişime geçecektir.
 
                         Ayrıca, öğretmenlerle doğrudan iletişim kurmak için arama ve WhatsApp arama özelliklerimizi kullanabilirsiniz. Arama özelliği sayesinde öğretmenlerin profillerine hızlıca erişebilir, ihtiyaçlarınıza uygun öğretmenlerle anında iletişime geçebilirsiniz. WhatsApp araması ile öğretmenlerle güvenli bir ortamda, hızlı bir şekilde görüşme sağlayarak ders detaylarını konuşabilir, alternatif oluşturabilir ve aradığınız matematik öğretmenini kolayca bulabilirsiniz.
                     </p>
