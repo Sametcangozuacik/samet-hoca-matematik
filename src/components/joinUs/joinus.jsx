c38e23bc100c6b0f1011dcd0340891a55ab464f0
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2025-02-07 17:18:13 +0300

    Düzelt: JoinUs bileşenindeki metin biçimlendirmesini güncelle

--- src/components/joinUs/joinus.jsx
+++ src/components/joinUs/joinus.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./joinus.module.scss";
 
-
 export default function JoinUs() {
     return (
         <div className={styles.joinUs}>
-        <h2>Lokasyon Matematik Ekibine Katıl</h2>
-        <p>"Matematik alanında uzman ya da başarılı bir üniversite öğrencisi olarak ekibimize katılmak için aşağıdaki butonları kullanabilirsiniz!"
-        </p>
-        <a href="https://wa.me/905073195505" className={styles.mainSectionBoxButton}>Bize Ulaşın</a>
+            <h2>Lokasyon Matematik Ekibine Katıl</h2>
+            <p>&quot;Matematik alanında uzman ya da başarılı bir üniversite öğrencisi olarak ekibimize katılmak için aşağıdaki butonları kullanabilirsiniz!&quot;</p>
+            <a href="https://wa.me/905073195505" className={styles.mainSectionBoxButton}>Bize Ulaşın</a>
         </div>
     );
 }
