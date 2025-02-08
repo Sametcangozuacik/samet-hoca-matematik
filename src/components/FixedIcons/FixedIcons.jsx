36f040e6fb37668439c2dad443f8c604d9074134
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2025-02-09 00:47:47 +0300

    Düzelt: Instagram ikonu eklendi; stil güncellemeleri yapıldı

--- src/components/FixedIcons/FixedIcons.jsx
+++ src/components/FixedIcons/FixedIcons.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Whatsapp from '../../../public/FixedIcons/whatsapplogo.svg';
 import Phone from '../../../public/FixedIcons/phonelogo.svg';
+import Instagram from '../../../public/FixedIcons/instagramlogo.svg';
 import styles from './FixedIcons.module.scss';
 
 const FixedIcons = () => {
@@ -20,6 +21,15 @@ const FixedIcons = () => {
         <Image 
           src={Whatsapp} 
           alt="WhatsApp" 
+          className={styles.icon} 
+          width={40} 
+          height={40} 
+        />
+      </a>
+      <a  href="https://www.instagram.com/lokasyonmatematik?igsh=MW10bmFnN25xODVtdw%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
+        <Image 
+          src={Instagram} 
+          alt="Instagram" 
           className={styles.icon} 
           width={40} 
           height={40} 
