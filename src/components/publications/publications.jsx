dc3cc87ac0e362bbaabefe232770e37a3cad296c
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2025-02-18 12:48:47 +0300

    Düzelt: Publications bileşeninin stil dosyasında düzenlemeler yapıldı; responsive tasarım iyileştirildi ve logo sınıfı güncellendi

--- src/components/publications/publications.jsx
+++ src/components/publications/publications.jsx
@@ -15,14 +15,14 @@ export default function Publications() {
         <div className={styles.publicationsContainer}>
             <h1 className={styles.title}>Kullandığımız Yayınlar</h1>
             <div className={styles.publicationsBox}>
-                <Image src={karekok} width={200} height={70} alt="Karekök Yayınları" className={styles.karekok} />
-                <Image src={ozdemir} width={200} height={70} alt="Özdemir Yayınları" className={styles.ozdemir} />
-                <Image src={fenomen} width={200} height={70} alt="Fenomen Yayınları" className={styles.fenomen} />
-                <Image src={bilgısarmal} width={200} height={70} alt="Bilgi Sarmal Yayınları" className={styles.bilgısarmal} />
-                <Image src={orginal} width={200} height={70} alt="Orjinal Yayınları" className={styles.orginal} />
-                <Image src={ucdortbeslogo} width={200} height={70} alt="Orjinal Yayınları" className={styles.orginal} />
-                <Image src={antreman} width={200} height={70} alt="Orjinal Yayınları" className={styles.orginal} />
-                <Image src={hız} width={200} height={70} alt="Orjinal Yayınları" className={styles.orginal} />
+                <Image src={karekok} width={200} height={70} alt="Karekök Yayınları" className={styles.publicationsBoxLogo} />
+                <Image src={ozdemir} width={200} height={70} alt="Özdemir Yayınları" className={styles.publicationsBoxLogo} />
+                <Image src={fenomen} width={200} height={70} alt="Fenomen Yayınları" className={styles.publicationsBoxLogo} />
+                <Image src={bilgısarmal} width={200} height={70} alt="Bilgi Sarmal Yayınları" className={styles.publicationsBoxLogo} />
+                <Image src={orginal} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
+                <Image src={ucdortbeslogo} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
+                <Image src={antreman} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
+                <Image src={hız} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
             </div>
         </div>
     );
