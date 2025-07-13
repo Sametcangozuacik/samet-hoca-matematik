17cf470c4e9e2128decdc93ecacb0b93791aa036
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2025-07-13 19:52:49 +0300

    Düzelt: 345logo.png dosyası silindi; 345logo.svg dosyası eklendi ve yayınlar bileşeninde logo güncellemeleri yapıldı; stil dosyasında responsive iyileştirmeler yapıldı

--- src/components/publications/publications.jsx
+++ src/components/publications/publications.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 import styles from "./publications.module.scss";
 import Image from "next/image";
+
 import karekok from "../../../public/publications/karekoklogo.png";
 import ozdemir from "../../../public/publications/ozdemırlogo.png";
 import fenomen from "../../../public/publications/fenomenlogo.png";
 import bilgısarmal from "../../../public/publications/bılgısarmallogo.png";
 import orginal from "../../../public/publications/orgınallogo.png";
-import ucdortbeslogo from "../../../public/publications/345logo.png";
+import ucdortbeslogo from "../../../public/publications/345logo.svg";
 import antreman from "../../../public/publications/antremanlogo.png";
 import hız from "../../../public/publications/hızlogo.svg";
 
+const publications = [
+  { src: karekok, alt: "Karekök Yayınları" },
+  { src: ozdemir, alt: "Özdemir Yayınları" },
+  { src: fenomen, alt: "Fenomen Yayınları" },
+  { src: bilgısarmal, alt: "Bilgi Sarmal Yayınları" },
+  { src: orginal, alt: "Orjinal Yayınları" },
+  { src: ucdortbeslogo, alt: "345 Yayınları" },
+  { src: antreman, alt: "Antrenman Yayınları" },
+  { src: hız, alt: "Hız Yayınları" },
+];
+
 export default function Publications() {
-    return (
-        <div className={styles.publicationsContainer}>
-            <h1 className={styles.title}>Kullandığımız Yayınlar</h1>
-            <div className={styles.publicationsBox}>
-                <Image src={karekok} width={200} height={70} alt="Karekök Yayınları" className={styles.publicationsBoxLogo} />
-                <Image src={ozdemir} width={200} height={70} alt="Özdemir Yayınları" className={styles.publicationsBoxLogo} />
-                <Image src={fenomen} width={200} height={70} alt="Fenomen Yayınları" className={styles.publicationsBoxLogo} />
-                <Image src={bilgısarmal} width={200} height={70} alt="Bilgi Sarmal Yayınları" className={styles.publicationsBoxLogo} />
-                <Image src={orginal} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
-                <Image src={ucdortbeslogo} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
-                <Image src={antreman} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
-                <Image src={hız} width={200} height={70} alt="Orjinal Yayınları" className={styles.publicationsBoxLogo} />
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.publicationsContainer}>
+      <h1 className={styles.title}>Kullandığımız Yayınlar</h1>
+      <div className={styles.publicationsBox}>
+        {publications.map(({ src, alt }, index) => (
+          <Image
+            key={index}
+            src={src}
+            width={220}
+            height={45}
+            alt={alt}
+            className={styles.publicationsBoxLogo}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
+
